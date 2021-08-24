8e868a4929c04a4e707edbf72ec5c2fa9c25b986
p_arkk <p_arkk@ai-dapps.com>
2021-08-24 23:45:55 +0900

    map 메소드 학습

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from "react";
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "apple",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Pink_lady_and_cross_section.jpg/353px-Pink_lady_and_cross_section.jpg",
+  },
+  {
+    name: "kimchi",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Various_kimchi.jpg/330px-Various_kimchi.jpg",
+  },
+  {
+    name: "juice",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Orange_juice_1.jpg/330px-Orange_juice_1.jpg",
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="sam" />
-      <Food fav="chu" />
+      {foodILike.map((list) => {
+        return <Food name={list.name} picture={list.image} />;
+      })}
     </div>
   );
 }
